ad1f0052ded2b4b716b4993802319437ac8c0164
thienphamstu <52187201+0356581777@users.noreply.github.com>
2019-11-28 13:41:04 +0700

    lam quản lý chức năng category

---
+++ resources/views/backend/page/category/insert.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.layoutadmin')
+@section('title','Quản Lý Sản Phẩm')
+@section('content')
+@section('session_header')
+
+{{--  <--!Vùng dataTables --!>  --}}
+<link rel="stylesheet" href="{{asset('css/jquery.dataTables.min.css') }}" >
+
+@endsection
+
+<section class="clearfix maincontent">
+        <form  action="{{ route('category_postinsert') }}"  method="post" role="form" enctype="multipart/form-data">
+                 {{ csrf_field() }}
+    <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <strong class="text-danger">
+                            Thêm Loại Sản Phẩm
+                        </strong>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <button type="submit" class="btn btn-success" ><i class="far fa-save"></i>Lưu[Thêm]</button>
+
+                            <a class="btn btn-sm btn-danger" href="{{ route('index_category') }}"><i class="fas fa-sign-out-alt"></i>Quay Lại
+
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="card-body">
+                @includeIf('backend.modules.message')
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="form-group">
+                            <label>Tên Loại sản Phẩm</label>
+                            <input name="name" class="form-control" type="text" value="{{ old('name') }}">
+                            @if ($errors->has('name'))
+
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                                <label>Mô Tả Seo</label>
+                                <textarea name="metadesc" class="form-control" rows="2" value="{{ old('metadesc') }}" ></textarea>
+                        </div>
+                        <div class="form-group">
+                                <label>Từ Khóa Seo</label>
+                                <textarea name="metakey" class="form-control" rows="2" value="{{ old('metakey') }}" ></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+
+
+                            <div class="form-group">
+                                    <label>Trạng Thái</label>
+                                    <select name="status" class="form-control">
+                                        <option value="1">Xuất Bán</option>
+                                        <option value="2">Chưa Xuất Bán</option>
+                                    </select>
+                                </div>
+
+
+                        </div>
+                </div>
+
+            </div>
+    </div>
+    </div>
+</form>
+
+</section>
+
+
+@endsection
+@section('session_footer')
+
+<script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+</script>
+<script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
+
+
+@endsection
+
+
